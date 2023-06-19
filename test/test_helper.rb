1843433da8f172d5165c36863fe62d0516e799ac
qvantt <2275399@gmail.com>
2023-06-19 11:08:46 +0300

    deps

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
 require "simplecov"
 require "minitest/autorun"
-# require "webmock"
+require "webmock/minitest"
 require "ipgeobase"
 
 SimpleCov.start do
